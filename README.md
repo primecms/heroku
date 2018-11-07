10fec10817c09767753bc447e5f94778305a31f1
Birkir Gudjonsson <birkir.gudjonsson@gmail.com>
2018-11-07 18:01:59 -0500

    Small update on env

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Prismic-like dynamic content type graphql generator.
 ```bash
 git clone git@github.com:birkir/prime-backend.git
 npm install
-npm run dev
+npm start
 ```
 
 ## TODO
--- src/sequelize.ts
+++ src/sequelize.ts
@@ -1,10 +1,18 @@
 import { Sequelize } from 'sequelize-typescript';
 
+let config = {} as any;
+
+if (process.env.DATABASE_URL) {
+  config.url = process.env.DATABASE_URL;
+} else {
+  config.database = 'prime-sq';
+  config.username = 'birkir';
+  config.password = '';
+}
+
 export const sequelize = new Sequelize({
   dialect: 'postgres',
-  database: 'prime-sq',
-  username: 'birkir',
-  password: '',
   modelPaths: [`${__dirname}/models`],
   logging: true,
+  ...config,
 });
